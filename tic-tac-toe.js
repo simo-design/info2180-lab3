4588645fc3f79ad0a59b2b89d7e6e28ad71b615b
simo-design <nadrine.simms@mymona.uwi.edugit config --global user.name simo-designgit config --global user.name simo-designgit config --global user.email nadrine.simms@mymona.uwi.edu>
2020-10-10 02:19:23 -0500

    Added script

---
+++ tic-tac-toe.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const x_class = 'x';
+const o_class ='o';
+const square = document.querySelectorAll('#board');
+const board = document.getElementById('#board'); 
+const btn = document.getElementById('.btn');
+
+function startGame(){
+btn.addEventListener('click');
+square.classList.addEventListener("")
+
+}
