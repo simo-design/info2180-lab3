8a20e69571f2f720033f0d1f7fccaa46135dee9e
simo-design <nadrine.simms@mymona.uwi.edugit config --global user.name simo-designgit config --global user.name simo-designgit config --global user.email nadrine.simms@mymona.uwi.edu>
2020-10-11 00:23:28 -0500

    Edited script

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,14 +1,15 @@
 window.onload = function(){
   
   const board = document.getElementById("#board");
-  let square = document.getElementById("div");
+  let square = [document.getElementById("#board > div")];
 
 
-  document.querySelector("#board").className = ("board");
+  document.querySelector("#board > div").className = ("square");
 
-  let e = querySelectorAll("board > div");
 
-  for(e=0; e<=9; e++){
+
+  for(let e = 0; e <= square.length; e++){
     document.querySelector("#board").classList.add("square");
+
   }
 }
